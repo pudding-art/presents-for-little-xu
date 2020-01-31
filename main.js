de97cbde0ca548dbdbdcde59d508311418af9c4b
pudding-art <60512507+pudding-art@users.noreply.github.com>
2020-01-31 22:55:20 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,177 @@
+;(function () {
+	
+	'use strict';
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Main Menu Superfish
+	var mainMenu = function() {
+
+		$('#fh5co-primary-menu').superfish({
+			delay: 0,
+			animation: {
+				opacity: 'show'
+			},
+			speed: 'fast',
+			cssArrows: true,
+			disableHI: true
+		});
+
+	};
+
+	// Parallax
+	var parallax = function() {
+		if ( !isiPad() || !isiPhone() ) {
+			$(window).stellar();
+		}
+	};
+
+
+	// Offcanvas and cloning of the main menu
+	var offcanvas = function() {
+
+		var $clone = $('#fh5co-menu-wrap').clone();
+		$clone.attr({
+			'id' : 'offcanvas-menu'
+		});
+		$clone.find('> ul').attr({
+			'class' : '',
+			'id' : ''
+		});
+
+		$('#fh5co-page').prepend($clone);
+
+		// click the burger
+		$('.js-fh5co-nav-toggle').on('click', function(){
+
+			if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			} else {
+				$('body').addClass('fh5co-offcanvas');
+			}
+			// event.preventDefault();
+
+		});
+
+		$('#offcanvas-menu').css('height', $(window).height());
+
+		$(window).resize(function(){
+			var w = $(window);
+
+
+			$('#offcanvas-menu').css('height', w.height());
+
+			if ( w.width() > 769 ) {
+				if ( $('body').hasClass('fh5co-offcanvas') ) {
+					$('body').removeClass('fh5co-offcanvas');
+				}
+			}
+
+		});	
+
+	}
+
+	
+
+	// Click outside of the Mobile Menu
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			}
+	    }
+		});
+	};
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 50, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+	
+	var stickyBanner = function() {
+		var $stickyElement = $('.sticky-banner');
+		var sticky;
+		if ($stickyElement.length) {
+		  sticky = new Waypoint.Sticky({
+		      element: $stickyElement[0],
+		      offset: 0
+		  })
+		}
+	};
+
+	// 比较的初始日期
+	var countDownDate = new Date("2019,10,30,18,00,00").getTime();
+
+	// 得到现在的时间从1970.1.1到现在的时间,now-1970.1.1
+	var now = new Date().getTime();
+
+	// Find the distance between now an the count down date
+	var distance = now - countDownDate;
+
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Display the result in an element with id="demo"
+	// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
+	// + minutes + "Minutes " + seconds + "Seconds ";
+
+	// Display the result in an element with id="demo"
+	document.getElementById("days").innerHTML = days +" <small>days</small>";
+	document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
+	document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
+	document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+
+	
+
+	// Document on load.
+
+	$(function(){
+		mainMenu();
+		parallax();
+		offcanvas();
+		mobileMenuOutsideClick();
+		contentWayPoint();
+		stickyBanner();
+	});
+
+
